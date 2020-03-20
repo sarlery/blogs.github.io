3e49452bd1445a589581d00054a075214099f112
sarlery <643360052@qq.com>
2020-03-20 12:02:38 +0800

    JavaScript 变量提升、闭包

--- JavaScript/语言特性/index.js
+++ JavaScript/语言特性/index.js
@@ -1,13 +1,47 @@
-var str = "abc_def_ghi";  // ---> abcDefGhi
+function find(ary, target){
+  var len = ary.length;
+  var res = "";
+  for(let i = 0;i < len;i ++){
+    var a = target - ary[i];
+    var idx = ary.indexOf(a);
+    if(idx !== -1){
+      res = res + ary[i] + ' ' + a;
+      break;
+    }
+  }
+  return res;
+}
 
-function humpStyle(str){
-  var reg = /([a-zA-Z]+)_+([a-zA-Z])([a-zA-Z]+)/g;
-  var newStr = str.replace(reg, function(match, p1, p2, p3){
-    var upperCase = p2.toLocaleUpperCase();
-    return p1 + upperCase + p3;
-  });
+// console.log(find([1,3,4,6,8], 10));
 
-  return newStr;
-} 
+// 去除括号
+function matchBracket(str){
+  var reg = /(\(\)|(\{\})|(\[\]))/g;
+  var len = str.length;
+  if(len % 2 !== 0){
+    return false;
+  }
+  var count = len / 2;
+  newStr = str;
+  while(count){
+    var newStr = newStr.replace(reg, "");
+    count -= 1;
+  }
+  return newStr ? false : true;
+}
 
-console.log(humpStyle(str));
+// console.log(matchBracket("[(){}](){(((((((((((((((((((((((())))))))))))))))))))))))}"));
+
+function fn(n){
+  var cache = [0, 1, 2];
+  function _fn(n){
+    if(cache[n]){
+      return cache[n];
+    }
+    cache[n] = _fn(n - 1) + _fn(n - 2);
+    return cache[n];
+  }
+  return _fn(n);
+}
+
+// console.log(fn(3));
--- JavaScript/语言特性/test.js
+++ JavaScript/语言特性/test.js
@@ -1,13 +1,22 @@
-const Promise = require("./promise");
+// const Promise = require("./promise");
 
-function test(param){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(param);
-        },1000);
-    });
+// function test(param){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(param);
+//         },1000);
+//     });
+// }
+
+// var t = test(100);
+
+// t.then(n => console.log("n == ",n), e => console.log("e == ",e));
+
+function fn(n){
+    if(true){
+        var a = "hello";
+    }
+    console.log(a);
 }
 
-var t = test(100);
-
-t.then(n => console.log("n == ",n), e => console.log("e == ",e));
+fn(3);
